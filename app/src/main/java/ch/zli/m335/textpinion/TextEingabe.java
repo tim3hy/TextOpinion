4668391eeef55ecbef2676d864bc1b0e0a411009
tim3hy <tim.christophe.graf@gmail.com>
2020-07-02 11:19:51 +0200

    commit

---
+++ app/src/main/java/ch/zli/m335/textpinion/TextEingabe.java
@@ -0,0 +1,46 @@
+package ch.zli.m335.textpinion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+public class TextEingabe extends AppCompatActivity {
+
+    EditText textEingabe;
+    Button txtPinion;
+    ImageButton imageButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_text_eingabe);
+
+        textEingabe = (EditText) findViewById(R.id.textEingabe);
+        txtPinion = (Button) findViewById(R.id.txtPinion);
+        imageButton = (ImageButton) findViewById(R.id.imageButton);
+
+        textEingabe.setHint("Text eingeben");
+        //Image Button
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentMainActiv= new Intent(TextEingabe.this, MainActivity.class);
+                startActivity(intentMainActiv);
+            }
+        });
+
+        txtPinion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentTxtPinion = new Intent(TextEingabe.this, TextPinion.class);
+                startActivity(intentTxtPinion);
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/ch/zli/m335/textpinion/TextPinion.java
@@ -0,0 +1,29 @@
+package ch.zli.m335.textpinion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class TextPinion extends AppCompatActivity {
+
+    ImageButton imageButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_text_pinion);
+
+        imageButton = (ImageButton) findViewById(R.id.imageButton);
+
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentTxtPinion = new Intent(TextPinion.this, TextEingabe.class);
+                startActivity(intentTxtPinion);
+            }
+        });
+
+    }
+}
